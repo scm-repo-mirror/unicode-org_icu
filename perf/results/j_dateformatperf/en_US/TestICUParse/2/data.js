401d31271406ae9615b645393544f7efb5ce7bbe
github-action-benchmark <github@users.noreply.github.com>
2025-09-24 21:18:27 +0000

    add Benchmark (ndjson) benchmark result for fff7c58b18e93a77eea3999764d66745e649d122

--- perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758745179644,
+  "lastUpdate": 1758748707832,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -39027,6 +39027,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 138.60992033293627,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "y.umaoka@gmail.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "committer": {
+            "email": "yumaoka@users.noreply.github.com",
+            "name": "Yoshito Umaoka",
+            "username": "yumaoka"
+          },
+          "distinct": true,
+          "id": "fff7c58b18e93a77eea3999764d66745e649d122",
+          "message": "ICU-23157 Do not use GMT Zero Format\n\nSee #3657",
+          "timestamp": "2025-09-24T16:58:41-04:00",
+          "tree_id": "dac327f6ea8e789dbbef770e0cf04c8e7e1845ab",
+          "url": "https://github.com/unicode-org/icu/commit/fff7c58b18e93a77eea3999764d66745e649d122"
+        },
+        "date": 1758748374681,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 138.74341625780835,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }

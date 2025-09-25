f5a5a76c51feff92d2dd16bbb55dacbc1dbd59be
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 18:31:25 +0000

    add Benchmark (ndjson) benchmark result for 122e60c6bedfff25476db4ceaa777af22258c964

--- perf/results/j_dateformatperf/sw_KE/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/sw_KE/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758820044851,
+  "lastUpdate": 1758825085266,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -39537,6 +39537,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 374.3451220220467,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "elango@unicode.org",
+            "name": "Elango Cheran",
+            "username": "echeran"
+          },
+          "distinct": true,
+          "id": "122e60c6bedfff25476db4ceaa777af22258c964",
+          "message": "ICU-23056 Update BRS commit checker to recognize done resolution status",
+          "timestamp": "2025-09-25T11:20:32-07:00",
+          "tree_id": "1b30a9bfaf201a05109e829f201b44afbf6bb290",
+          "url": "https://github.com/unicode-org/icu/commit/122e60c6bedfff25476db4ceaa777af22258c964"
+        },
+        "date": 1758824628669,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 353.37001572853455,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }

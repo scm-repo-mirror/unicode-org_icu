e02960903a2a9d653010a46b84ae7719c01dc602
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 10:19:26 +0000

    add Benchmark (ndjson) benchmark result for f00a17b5bada744f8d2ff8ff93470457a9b75dce

--- perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
+++ perf/results/j_dateformatperf/en_US/TestICUParse/1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758769505289,
+  "lastUpdate": 1758795566046,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -39297,6 +39297,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICUParse",
             "value": 348.1301642192541,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dragan@unicode.org",
+            "name": "DraganBesevic",
+            "username": "DraganBesevic"
+          },
+          "committer": {
+            "email": "younies.mahmoud@gmail.com",
+            "name": "Younies Mahmoud",
+            "username": "younies"
+          },
+          "distinct": true,
+          "id": "f00a17b5bada744f8d2ff8ff93470457a9b75dce",
+          "message": "ICU-23056 Integrate CLDR 48 release beta1",
+          "timestamp": "2025-09-25T11:48:23+02:00",
+          "tree_id": "6dd339d2390153138b6d8b8056787efe8f393f8f",
+          "url": "https://github.com/unicode-org/icu/commit/f00a17b5bada744f8d2ff8ff93470457a9b75dce"
+        },
+        "date": 1758795176400,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICUParse",
+            "value": 334.85180951660436,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }

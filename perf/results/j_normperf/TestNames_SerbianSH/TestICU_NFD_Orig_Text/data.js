905c691cf78b3aedf79366a664ab61cb9b60b92a
github-action-benchmark <github@users.noreply.github.com>
2025-09-29 22:38:48 +0000

    add Benchmark (ndjson) benchmark result for d10d79cb7531543964beaa3469b6dc10cba74e1d

--- perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_Orig_Text/data.js
+++ perf/results/j_normperf/TestNames_SerbianSH/TestICU_NFD_Orig_Text/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759180379672,
+  "lastUpdate": 1759185528266,
   "repoUrl": "https://github.com/unicode-org/icu",
   "entries": {
     "Benchmark": [
@@ -40107,6 +40107,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "TestICU_NFD_Orig_Text",
             "value": 9.572457105558172,
+            "unit": "ns/iter",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cwcornelius@gmail.com",
+            "name": "sven-oly",
+            "username": "sven-oly"
+          },
+          "committer": {
+            "email": "cwcornelius@gmail.com",
+            "name": "Craig Cornelius",
+            "username": "sven-oly"
+          },
+          "distinct": true,
+          "id": "d10d79cb7531543964beaa3469b6dc10cba74e1d",
+          "message": "ICU-23056 ICU 78 scrub issues\n\nSee #3697",
+          "timestamp": "2025-09-29T14:59:11-07:00",
+          "tree_id": "78dedac5ada528c0db700b09061fa3bbe651a75a",
+          "url": "https://github.com/unicode-org/icu/commit/d10d79cb7531543964beaa3469b6dc10cba74e1d"
+        },
+        "date": 1759185149677,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "TestICU_NFD_Orig_Text",
+            "value": 8.562158108924796,
             "unit": "ns/iter",
             "biggerIsBetter": false
           }
